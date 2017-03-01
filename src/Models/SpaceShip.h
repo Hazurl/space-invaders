f652c713eb80504161e6f029753d6a18897b05ac
Hazurl <ricoujules69@gmail.com>
2017-03-01 17:40:09 +0100

    [SpaceShip] make spaceship inherit from GameObject

--- src/Models/SpaceShip.h
+++ src/Models/SpaceShip.h
@@ -4,47 +4,35 @@
 #include "../Utilities/Loader.h"
 #include "../Utilities/Define.h"
 #include "../Utilities/Settings.h"
+#include "GameObject.h"
 
 #include <string>
 #include <SFML/Graphics.hpp>
 
-class SpaceShip {
+class SpaceShip : public GameObject {
 public :
+/*              CONSTRUCTORS                                                    */
     SpaceShip (std::string const& imgName, float x = 0.0, float y = 0.0);
+
+/*              DESTRUCTOR                                                      */
     ~SpaceShip();
 
+/*              UPDATE                                                          */
     void update(float speed);
     void moveX(float amount);
     void moveY(float amount);
     void invertX();
 
+/*              SETTER                                                          */
     void setPosition (float x, float y);
     void setLife (unsigned int life);
 
-    sf::FloatRect getCollider ();
-
-    void setSpriteFromFile(std::string const& imgName);
-    void nextFrame();
-
-    void draw (sf::RenderWindow* window);
-
-    float getX();
-    float getY();
+/*              GETTER                                                          */
     unsigned int getLife();
 
 private :
-    float x = 0;
-    float y = 0;
     bool move_right = true;
-
     unsigned int life = 1;
-
-    sf::FloatRect collider;
-
-    unsigned int framesCount;
-    unsigned int currentFrame;
-
-    sf::Sprite sp;
 };
 
 #endif
