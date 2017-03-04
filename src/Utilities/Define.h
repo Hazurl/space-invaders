6a1f1cb759bbcc084b80e4e184108289fab055ee
Hazurl <ricoujules69@gmail.com>
2017-03-04 01:26:45 +0100

    [Define] Default invader's sprite pattern

--- src/Utilities/Define.h
+++ src/Utilities/Define.h
@@ -7,6 +7,9 @@
 #define HEIGHT 600
 #define SPEED 1
 
+#define DEFAULT_INV_PATTERN "abbcc" // pattern for invaders sprites
+// That means, there is one line of invaders A, 2 lines of invaders B, and 2 of invaders C
+
 #define IMAGE_PATH(x) "img/" x
 #define SETTINGS_PATH(x) "settings/" x
 #define FONT_PATH(x) "font/" x
